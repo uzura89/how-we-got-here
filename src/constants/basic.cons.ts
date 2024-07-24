dc76fbf0992e49333dbda40472c4983c5e899a9b
白水佑哉 <ys0520@gmail.com>
2024-07-24 20:06:38 +0900

    add ogp image

--- src/constants/basic.cons.ts
+++ src/constants/basic.cons.ts
@@ -1,4 +1,4 @@
 export const CONS_APP_DOMAIN = "hwgh.net";
 export const CONS_APP_URL = `https://${CONS_APP_DOMAIN}`;
 
-export const CONS_IMG_PATH_OGP = "/img/brand/ogpimage.png";
+export const CONS_IMG_PATH_OGP = `${CONS_APP_URL}/img/brand/ogpimage.png`;
